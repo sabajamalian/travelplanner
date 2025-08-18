69422a5e49a05dfcd326a1a5229ec8eed5746c11
Saba Jamalian <jamalian.saba@gmail.com>
2025-08-17 23:26:04 -0500

    Added a backend python database with healthcheck and connection to db

---
+++ server/routes/__init__.py
@@ -0,0 +1,20 @@
+"""
+API Routes Package
+
+This package contains all the API route definitions for the Travel Planner backend.
+Routes are organized by feature/domain and imported here for easy access.
+"""
+
+# Import route modules here as they are created
+# from .health import router as health_router
+# from .travels import router as travels_router
+# from .events import router as events_router
+# from .users import router as users_router
+
+# List of all routers for easy registration
+# __all__ = [
+#     "health_router",
+#     "travels_router", 
+#     "events_router",
+#     "users_router"
+# ]
